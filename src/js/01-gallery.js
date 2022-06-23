9b416d9a43adacfb1782be0a55bb1eb5e47a544f
Artem Chubenko <artem.chubenko@gmail.com>
2022-06-23 23:41:27 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,31 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
 
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
 
 function importGallery(images) {
-    const galleryItems = images.map(image =>
-
+  const galleryItems = images
+    .map(
+      image =>
         `<a class="gallery__item" href="${image.original}">
   <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
-</a>`).join("");    
-    gallery.insertAdjacentHTML('afterbegin' , galleryItems);  
-    
-};
+</a>`
+    )
+    .join('');
+  gallery.insertAdjacentHTML('afterbegin', galleryItems);
+}
 
 importGallery(galleryItems);
 
-const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt',
- captionDelay: 250,
- captionPosition: 'bottom',});
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
 
 // Change code below this line
-
-
-
-
 
 console.log(galleryItems);
